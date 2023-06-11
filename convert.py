dac6a8f59cd22d209e027d1e6d4d72597e34ae92
Kasen <18170166+imkasen@users.noreply.github.com>
2023-06-11 15:04:05 +0800

    parse multiple baisc <h3> + <a> folder structures
    update

--- convert.py
+++ convert.py
@@ -30,17 +30,22 @@ def check_file(input_path: str, output_path: str) -> None:
         sys.exit(f"Sorry, '{output_path}' already exists.")
 
 
-def convert2json(content: str) -> dict | None:
+def convert2json(content: str) -> dict:
     soup = BeautifulSoup(content, "html.parser")
 
     # Firefox bookmarks will provide a <H3> tag with a 'PERSONAL_TOOLBAR_FOLDER' attribute
     toolbar_dl_tag = soup.find("h3", attrs={"personal_toolbar_folder": "true"}).find_next_sibling("dl")
     dl_tag = toolbar_dl_tag.find("dl")
 
-    if dl_tag:
-        return parse_folders(dl_tag)
+    dir: dict = {}
+    while dl_tag:
+        tmp_dir: dict = parse_folders(dl_tag)
+        if tmp_dir:
+            # TODO: conflits that different dirs with the same name
+            dir.update(tmp_dir)
+        dl_tag = dl_tag.find_next_sibling().find("dl")
 
-    return None
+    return dir
 
 
 def parse_folders(tag) -> dict | None:
@@ -74,16 +79,16 @@ if __name__ == "__main__":
     input_path, output_path = parse_input()
     check_file(input_path, output_path)
     type: str = os.path.splitext(output_path.lower())[-1][1:]
-    dir: dict | None = None
 
     if type == "json":
-        with open(input_path, "r", encoding="utf-8") as input_file:
+        with open(input_path, "r", encoding="utf-8") as input_file, \
+             open(output_path, "w", encoding="utf-8") as output_file:
             content: str = input_file.read()
-            dir = convert2json(content)
-        if dir:
-            with open(output_path, "w", encoding="utf-8") as output_file:
-                json.dump(dir, output_file, ensure_ascii=False)
+            dir: dict = convert2json(content)
+            json.dump(dir, output_file, ensure_ascii=False)
     elif type == "csv":
         pass
-    else:  # md
+    elif type == "md":
         pass
+    else:
+        sys.exit("Unknown output file type!")
